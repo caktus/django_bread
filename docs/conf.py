3b549ab74326acdde6cd788a20bb4b26f927857c
Mariatta <Mariatta@users.noreply.github.com>
2025-07-09 13:30:05 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -189,7 +189,7 @@ html_static_path = []
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-# html_search_language = 'en'
+# html_search_language = "en"
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
